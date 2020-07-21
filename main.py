8dada144a5b93b19138cbf1148aa0ff990e540d9
David Skold <lemurman384@gmail.com>
2020-07-21 06:37:31 +0000

    collected and tested different unicode symbols for piece display

--- main.py
+++ main.py
@@ -3,3 +3,27 @@ from blessings import Terminal
 t = Terminal()
 print(t.red+"Battlecruiser operational")
 print(t.normal)
+
+def printBoard(board,row_offset=2,col_offset=4):
+  for r,row in enumerate(board):
+    for c,square in enumerate(row):
+      location = t.move(r+row_offset,c*2+col_offset)
+      square_color = t.on_bright_blue if (r+c)%2==1 else t.on_blue
+      piece = "\u23fa"
+      piece_color = t.bright_yellow
+      text = f"{location}{piece_color}{square_color}{piece:2}{t.normal}"
+      print(text)
+
+
+TEST_BOARD = [
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+]
+
+printBoard(TEST_BOARD)
